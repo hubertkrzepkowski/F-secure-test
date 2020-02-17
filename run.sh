677e8e0d84c69181a80db97578d579d6abdf7ff9
hubertkrzepkowski <hkrzepa@gmail.com>
2020-02-17 20:28:27 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+apt update
+apt upgrade
+apt install curl
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 if [ ! "$(docker ps -q -f name=webapp)" ]
 then
      docker-compose build
