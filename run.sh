d79accc7cd5fd99725f7b8a718e9c29b02b87db3
Hubert <hkrzepa@gmail.com>
2020-02-18 01:52:05 +0100

    script update

--- run.sh
+++ run.sh
@@ -3,12 +3,16 @@ apt update
 apt upgrade
 apt install curl
 apt install git
+if [ !-x "$(command -v docker)" ]; then
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
-curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+fi
+if [! -x "$(command -v docker-compose)" ]; then
+curl -L https://github.com/docker/compose/releases/download/1.25.3/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-git clone https://github.com/hubertkrzepkowski/F-secure-test.git
-cd F-secure-test
+fi
+#git clone https://github.com/hubertkrzepkowski/F-secure-test.git
+#cd F-secure-test
 if [ ! "$(docker ps -q -f name=webapp)" ]
 then
      docker-compose build
