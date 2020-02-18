16a32eaaa30667c23d8aa4091a8590b027d37c01
hubertkrzepkowski <hkrzepa@gmail.com>
2020-02-18 13:58:28 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,12 +7,12 @@ if [ !-x "$(command -v docker)" ]; then
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 fi
-if [! -x "$(command -v docker-compose)" ]; then
+if [ ! -x "$(command -v docker-compose)" ]; then
 curl -L https://github.com/docker/compose/releases/download/1.25.3/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 fi
-#git clone https://github.com/hubertkrzepkowski/F-secure-test.git
-#cd F-secure-test
+git clone https://github.com/hubertkrzepkowski/F-secure-test.git
+cd F-secure-test
 if [ ! "$(docker ps -q -f name=webapp)" ]
 then
      docker-compose build
